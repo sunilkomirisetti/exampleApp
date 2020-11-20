5e1369a459645935cfb5bbcb02c4f72b8b22c8a9
sunilkomirisetti <35510127+sunilkomirisetti@users.noreply.github.com>
2020-11-20 12:53:08 +0530

    Update scriptGen.js

--- apis/scriptGen/scriptGen.js
+++ apis/scriptGen/scriptGen.js
@@ -5,91 +5,18 @@ const _ = require('underscore');
 async function runScript(configObj, callback) {
   const result = [];
   console.log('lets begin the action ::: ');
-    const browser = await puppeteer.launch({headless: false});
-    let page = await browser.newPage();
-    
-    await page.setRequestInterception(true);
-    await page.setDefaultNavigationTimeout(0);
-
-    await page.on('request', request => {
-      request_client({
-        uri: request.url(),
-        resolveWithFullResponse: true,
-      }).then(response => {
-        //if (request.url().startsWith('https://smetrics.statefarm.com/b/ss')) {
-          let capturedData = {
-            'requestUrl' : request.url(),
-            'requestHeaders': request.headers(),
-            'requestPostData': request.postData(),
-            'responseHeaders': response.headers,
-            'responseSize': response.headers['content-length'],
-            'responseBody': response.body,
-          };
-          result.push(capturedData);
-          /*console.log('*************');
-          console.log('result :: ', result);
-          console.log('*************');*/
-        //}
-        request.continue();
-      }).catch(error => {
-        //console.error(error);
-        request.abort();
-      });
-    });
-    
-    let pageObjects = _.sortBy(configObj.pageObjects, 'pageIndex');
-    let lastIndex = pageObjects.length;
-    console.log('pageObjects sorted :: ', lastIndex)
-    for (i=0; i<lastIndex; i++) {
-      let pageObj = pageObjects[i];
-      console.log('going to :: ' + pageObj.pageURL);
-      await page.goto(pageObj.pageURL);
-      page.waitForTimeout(3000).then(() => console.log('Waited a second!'));
-      if (pageObj.formData && pageObj.formData.length) {
-        let formData = pageObj.formData;
-       formData.forEach(function(obj) {
-          if ('dropdown' == obj.type) {
-             page.select('#' + obj.key, obj.value);
-          } else if ('input' == obj.type) {
-             page.type('#' + obj.key, obj.value);
-          }
-        });
-        if ('Submit' == pageObj.pageAction) {
-            console.log('in SUBMIT :: ');
-            if (pageObj.actionEventType && 'selector' == pageObj.actionEventType) {
-                await Promise.all([await page.click('#' + pageObj.actionEventId)]);
-                callback({
-                  status: 'SUCCESS',
-                  result: result
-                });
-            } else {
-              await page.click('#' + pageObj.actionEventId);
-              callback({
-                status: 'SUCCESS',
-                result: result
-              });
-            }
-            
-            
-            
-        }
-      }
-    }
-}
-
-/*async function runScript(configObj, callback) {
-  const result = [];
-  console.log('lets begin the action ::: ');
   const browser = await puppeteer.launch({headless: false});
-  //Open new page/tab
-  const page = await browser.newPage();
-  //Setting timeout 0 to avoid any timeout issues
+  let page = await browser.newPage();
+    
+  await page.setRequestInterception(true);
   await page.setDefaultNavigationTimeout(0);
 
-
-  await page.on('response', async response => {
-    let request = response.request();
-      if (request.url().startsWith('https://smetrics.statefarm.com/b/ss')) {
+  await page.on('request', request => {
+    request_client({
+      uri: request.url(),
+      resolveWithFullResponse: true,
+    }).then(response => {
+      //if (request.url().startsWith('https://smetrics.statefarm.com/b/ss')) {
         let capturedData = {
           'requestUrl' : request.url(),
           'requestHeaders': request.headers(),
@@ -99,39 +26,90 @@ async function runScript(configObj, callback) {
           'responseBody': response.body,
         };
         result.push(capturedData);
-        console.log('*************');
-        console.log(request.method());
-        console.log('*************');
-      }
+        /*console.log('*************');
+        console.log('result :: ', result);
+        console.log('*************');*/
+      //}
       request.continue();
+    }).catch(error => {
+      //console.error(error);
+      request.abort();
     });
+  }); 
 
-
-  let pageObjects = _.sortBy(configObj.pageObjects, 'pageIndex');
-  let lastIndex = pageObjects.length;
-  console.log('pageObjects sorted :: ', lastIndex)
-  for (var i=0; i<lastIndex; i++) {
-    let pageObj = pageObjects[i];
-    console.log('going to :: ' + pageObj.pageURL);
-    await page.goto(pageObj.pageURL);
-    if (pageObj.formData && pageObj.formData.length) {
+  (async() => {
+    let pageObjects = _.sortBy(configObj.pageObjects, 'pageIndex');
+    let lastIndex = pageObjects.length;
+    console.log('pageObjects sorted :: ', lastIndex)
+    for (i=0; i<lastIndex; i++) {
+      let pageObj = pageObjects[i];
+      console.log('going to :: ' + pageObj.pageURL);
+      await page.goto(pageObj.pageURL);
+      page.waitForNavigation();
+       page.$eval('#quote-main-zip-btn', elem => elem.click());
       let formData = pageObj.formData;
-      formData.forEach(function(obj) {
-        if ('dropdown' == obj.type) {
-           page.select('#' + obj.key, obj.value);
-        } else {
-           page.type('#' + obj.key, obj.value);
-        }
-      });
-      if ('Submit' == pageObj.pageAction) {
-          await page.click('#' + pageObj.actionEventId);
-          callback({
-            status: 'SUCCESS',
-            result: result
+      if (formData && formData.length) {
+
+        (async() => {
+          formData.forEach(function(obj) {
+            if ('dropdown' == obj.type) {
+               page.select('#' + obj.key, obj.value);
+            } else if ('input' == obj.type) {
+               page.type('#' + obj.key, obj.value);
+            }
           });
+          if ('Submit' == pageObj.pageAction) {
+              console.log('in SUBMIT :: ');
+              //page.$eval('#quote-main-zip-btn', elem => elem.click());
+              if (pageObj.actionEventType && 'selector' == pageObj.actionEventType) {
+                  Promise.all([ page.click('#' + pageObj.actionEventId)]);
+                  callback({
+                    status: 'SUCCESS',
+                    result: result
+                  });
+              } else {
+                console.log('in else :: ');
+                 page.click('#' + pageObj.actionEventId);
+                 /*page.click('#quote-main-zip-btn');
+                 page.keyboard.press('Enter');
+                 page.type('#quote-main-zip-code-input',String.fromCharCode(13));*/
+                 //page.$eval('#quote-main-zip-btn', elem => elem.click());
+                 console.log('after click');
+                 page.waitForNavigation();
+                callback({
+                  status: 'SUCCESS',
+                  result: result
+                });
+              }
+          }
+        })();
       }
     }
-  }
-}*/
+    //page.click('#quote-main-zip-btn');
+    console.log('this will print last');
+  })();
+}
+
+async function setFormData(formData, page, callback) {
+  let lastIndex = formData.length;
+  formData.forEach(function(obj) {
+    if ('dropdown' == obj.type) {
+       page.select('#' + obj.key, obj.value);
+       lastIndex = lastIndex - 1;
+       if (lastIndex <= 0) {
+        page.keyboard.press('Enter');
+        callback({'status': 'completed'})
+       }
+    } else if ('input' == obj.type) {
+       page.type('#' + obj.key, obj.value);
+       lastIndex = lastIndex - 1;
+       if (lastIndex <= 0) {
+        page.keyboard.press('Enter');
+        callback({'status': 'completed'})
+       }
+    }
+  });
+}
+
 
 module.exports.runScript = runScript;
