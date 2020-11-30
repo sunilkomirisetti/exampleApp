44cd07c04ec6417802a395c33ff5bfdba7797d19
sunilkomirisetti <35510127+sunilkomirisetti@users.noreply.github.com>
2020-11-30 15:42:42 +0530

    Update testReport.js

--- apis/testReport/testReport.js
+++ apis/testReport/testReport.js
@@ -60,10 +60,9 @@ function createAndRunScript(configId, callback) {
 		} else {
 			let configObj = response.result[0];
 			if (configObj) {
-				console.log('configObj :: ', configObj);
 				ScriptUtil.runScript(configObj, function(response) {
 					let fileName;
-					let filePath = './uploads';
+					let filePath = './';
 					let counter = 1;
 					let currentRow = 0;
 					let startRow = 1;
@@ -72,8 +71,8 @@ function createAndRunScript(configId, callback) {
 					let yyyy = date.getFullYear().toString(),
 					    MM = ("0" + (date.getMonth() + 1)).slice(-2),
 					    dd = ("0" + (date.getDate())).slice(-2),
-					    hh = (" 0" + (date.getHours())).slice(-2) + ('_'),
-					    mm = ("0" + (date.getMinutes())).slice(-2) + '_',
+					    hh = (" 0" + (date.getHours())).slice(-2) + ('-'),
+					    mm = ("0" + (date.getMinutes())).slice(-2) + '-',
 					    ss = ("0" + (date.getSeconds())).slice(-2);
 
 					fileName = "./" + yyyy + MM + dd+  hh + mm + ss + ".xlsx";
@@ -91,29 +90,137 @@ function createAndRunScript(configId, callback) {
 					];
 					currentRow = currentRow + 1;
 					startRow = startRow + 1;
-
-					if (response.result && response.result.length) {
+					let result = response.result;
+					if (result && result.length) {
+						console.log('captured data :: ', result);
 						//script captured data-- iterate it and build report file
-
-        		       	workbook.xlsx.writeFile(fileName)
-        		         .then(() => {
-   			        	  	callback({
-     								fileName: fileName,
-     								filePath: fileName
-     							});
-        		           return;
-        		       }).catch(error => {
-							console.log('before sending response :: EXCEPTION', error);              
-			        	  	callback({
-  								fileName: fileName,
-  								filePath: fileName
+						let lastIndex = result.length;
+						console.log('lastIndex :: ' + lastIndex);
+						result.forEach(function(obj) {
+							let index = 1;
+							let queryParams = obj.queryParams;
+							if (obj.queryParams) {
+								let lastQueryParamIndex = Object.keys(obj.queryParams).length;
+								console.log('lastQueryParamIndex :: ', lastQueryParamIndex);
+								(async() => {
+									Object.keys(obj.queryParams).forEach(function(param) {
+										if (1 == index) {
+										  worksheet.addRow({'sNo': counter ++, 'requestUrl': obj.requestUrl, 'requestType': obj.requestType, 'requestHeaders':  JSON.stringify(obj.requestHeaders, null, 2) + '', 'requestPostData': JSON.stringify(obj.requestPostData, null, 2), 'requestQueryParams': param + ' : ' + queryParams[param], 'responseHeaders': JSON.stringify(obj.responseHeaders, null, 2) + '', 'responseSize': obj.responseSize + '', responseBody: JSON.stringify(obj.responseBody, null, 2) + ''});
+										  index = index + 1;
+										  currentRow = currentRow + 1;
+										} else {
+										  worksheet.addRow({'sNo': '', 'requestUrl': '', 'requestType': '', 'requestHeaders':  '', 'requestPostData': '', 'requestQueryParams': param + ' : ' + queryParams[param], 'responseHeaders': '', 'responseSize': '', responseBody: ''});
+										  currentRow = currentRow + 1;
+										  index = index + 1;
+										}          
+									});
+								})();
+							  // merge by start row, start column, end row, end column
+							  	try {
+							  		worksheet.mergeCells(startRow, 1, currentRow, 1);
+							  		worksheet.mergeCells(startRow, 2, currentRow, 2);
+							  		worksheet.mergeCells(startRow, 3, currentRow, 3);
+							  		worksheet.mergeCells(startRow, 4, currentRow, 4);
+							  		worksheet.mergeCells(startRow, 5, currentRow, 5);
+							  		worksheet.mergeCells(startRow, 7, currentRow, 7);
+							  		worksheet.mergeCells(startRow, 8, currentRow, 8);
+							  		startRow = startRow + index - 1;
+							  	} catch(e) {console.log('error happening while merging :: ', startRow, currentRow);}
+							    
+							    lastIndex = lastIndex - 1;
+							    console.log('lastIndex :: ' + lastIndex);
+							    if (lastIndex <= 0) {
+	    	        		       	workbook.xlsx.writeFile(fileName)
+	    	        		         .then(() => {
+	    	   			        	  	callback({
+	    	     								fileName: fileName,
+	    	     								filePath: fileName
+	    	     							});
+	    	        		           return;
+	    	        		       	}).catch(error => {
+	    								console.log('before sending response :: EXCEPTION', error);              
+	    				        	  	callback({
+	    	  								fileName: fileName,
+	    	  								filePath: fileName
+	    	  							});
+	    	  							return;
+	    							});
+	    	        		       	return;
+							    }
+							} else if (obj.requestPostData) {
+							  console.log('POST ::: ');
+							  //Logic to be added for post
+							  let requestBody = obj.requestPostData.split('&');
+							  requestBody.forEach(function(param) {
+							    if (1 == index) {
+							      worksheet.addRow({'sNo': counter ++, 'requestUrl': obj.requestUrl, 'requestType': obj.requestType, 'requestHeaders':  JSON.stringify(obj.requestHeaders, null, 2), 'requestPostData': param.substring(0, param.indexOf('=')) + ' : ' + param.substring(param.indexOf('=') + 1), 'requestQueryParams': '', 'responseHeaders': JSON.stringify(obj.responseHeaders, null, 2), 'responseSize': obj.responseSize, responseBody: JSON.stringify(obj.responseBody, null, 2)});
+							      index = index + 1;
+							      currentRow = currentRow + 1;
+							    } else {
+							      worksheet.addRow({'sNo': '', 'requestUrl': '', 'requestType': '', 'requestHeaders':  '', 'requestPostData': param.substring(0, param.indexOf('=')) + ' : ' + param.substring(param.indexOf('=') + 1), 'requestQueryParams': '', 'responseHeaders': '', 'responseSize': '', responseBody: ''});
+							      currentRow = currentRow + 1;
+							      index = index + 1;
+							    }          
+							  });
+							  try {
+  							  		worksheet.mergeCells(startRow, 1, currentRow, 1);
+  							  		worksheet.mergeCells(startRow, 2, currentRow, 2);
+  							  		worksheet.mergeCells(startRow, 3, currentRow, 3);
+  							  		worksheet.mergeCells(startRow, 4, currentRow, 4);
+  							  		worksheet.mergeCells(startRow, 5, currentRow, 5);
+  							  		worksheet.mergeCells(startRow, 7, currentRow, 7);
+  							  		worksheet.mergeCells(startRow, 8, currentRow, 8);
+  							  		startRow = startRow + index - 1;
+  							  	} catch(e) {console.log('error happening while merging :: ', startRow, currentRow);}
+							  startRow = startRow + index - 1;
+						    lastIndex = lastIndex - 1;
+						    console.log('lastIndex :: ' + lastIndex);
+						    if (lastIndex <= 0) {
+  	        		       	workbook.xlsx.writeFile(fileName)
+  	        		         .then(() => {
+  	   			        	  	callback({
+  	     								fileName: fileName,
+  	     								filePath: fileName
+  	     							});
+  	        		           return;
+  	        		       	}).catch(error => {
+  								console.log('before sending response :: EXCEPTION', error);              
+  				        	  	callback({
+  	  								fileName: fileName,
+  	  								filePath: fileName
+  	  							});
+  	  							return;
   							});
-  							return;
+  	        		       	return;
+						    }
+
+							} else {
+							  worksheet.addRow({'sNo': counter ++, 'requestUrl': obj.requestUrl, 'requestType': obj.requestType, 'requestHeaders':  JSON.stringify(obj.requestHeaders, null, 2), 'requestPostData': JSON.stringify(obj.requestPostData, null, 2), 'requestQueryParams': JSON.stringify(obj.queryParams, null, 2), 'responseHeaders': JSON.stringify(obj.responseHeaders, null, 2), 'responseSize': obj.responseSize, 'responseBody': JSON.stringify(obj.responseBody, null, 2)});
+							    startRow = startRow + index - 1;
+    						    lastIndex = lastIndex - 1;
+    						    if (lastIndex <= 0) {
+        	        		       	workbook.xlsx.writeFile(fileName)
+        	        		         .then(() => {
+        	   			        	  	callback({
+        	     								fileName: fileName,
+        	     								filePath: fileName
+        	     							});
+        	        		           return;
+        	        		       	}).catch(error => {
+        								console.log('before sending response :: EXCEPTION', error);              
+        				        	  	callback({
+        	  								fileName: fileName,
+        	  								filePath: fileName
+        	  							});
+        	  							return;
+        							});
+        	        		       	return;
+    						    }
+							}
 						});
-        		       	return;
 					} else {
 						//no data captured -- send empty file
-
+						console.log('no result object found');
         		       	workbook.xlsx.writeFile(filePath)
         		         .then(() => {
    			        	  	callback({
