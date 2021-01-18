d8846084bbc7a81ea09924cb5ffaa713a0aa9ea1
sunilkomirisetti <35510127+sunilkomirisetti@users.noreply.github.com>
2021-01-18 22:33:53 +0530

    Update scriptGen.js

--- apis/scriptGen/scriptGen.js
+++ apis/scriptGen/scriptGen.js
@@ -4,21 +4,38 @@ const _ = require('underscore');
 const URL = require('url');
 const { PendingXHR } = require('pending-xhr-puppeteer');
 
-function runScript(configObj, callback) {
-  let result = [];
-  let pageObjConfig = {};
-  console.log('lets begin the action ::: ');
+let browser;
+let result = [];
 
-  (async() => {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
-  const pendingXHR = new PendingXHR(page);
-    
-  await page.setRequestInterception(true);
-  await page.setDefaultNavigationTimeout(0);
-  await page.setCacheEnabled(true);
+function scriptLauncher(configObj) {
+  return new Promise((resolve, reject) => {
+    (async() => {
+      browser = await puppeteer.launch({headless: false});
+      const page = await browser.newPage();
+      const pendingXHR = new PendingXHR(page);
+      let pageObjects = _.sortBy(configObj.pageObjects, 'pageIndex');
+      let lastIndex = pageObjects.length;
+      let forIndex = 0;
+      await page.setDefaultNavigationTimeout(0);
+      await page.setCacheEnabled(false);      
 
-  /*await page.on('response', async response => {
+      for (forIndex=0; forIndex<lastIndex; forIndex++) {
+        await pageGraber(page, pageObjects[forIndex]);
+        console.log('forIndex :: ', forIndex);
+        if (forIndex + 1 == lastIndex) {
+          resolve({
+            status: 'SUCCESS',
+            result: result
+          });
+        }
+      }            
+    })();
+  });
+}
+
+
+async function pageGraber(page, pageObj) {
+  await page.on('response', async response => {
     let request = response.request();
     let capturedData = {
       'requestUrl' : request.url(),
@@ -30,186 +47,81 @@ function runScript(configObj, callback) {
       'responseBody': response.body,
       'queryParams': URL.parse(request.url(), true).query
     };
-    result.push(capturedData);
-    console.log('*************');
-    console.log('result captured :: ');
-    console.log('*************');
-  });*/
-
-  await page.on('request', request => {
-    request_client({
-      uri: request.url(),
-      resolveWithFullResponse: true,
-    }).then(response => {
-        let capturedData = {
-          'requestUrl' : request.url(),
-          'requestType': request.method(),
-          'requestHeaders': request.headers(),
-          'requestPostData': request.postData(),
-          'responseHeaders': response.headers,
-          'responseSize': response.headers['content-length'],
-          'responseBody': response.body,
-          'queryParams': URL.parse(request.url(), true).query
-        };
-        if (pageObjConfig.captureURLs) {
-          if (_.some(pageObjConfig.captureURLs, function(url) { return request.url().indexOf(url) > -1 ? true : false;})) {
-            console.log('url matched :::')
-            if (pageObjConfig.captureMethods) {
-              console.log('methods :: ', pageObjConfig.captureMethods);
-              if (_.some(pageObjConfig.captureMethods, function(method) { return method == request.method();})) {
-                  result.push(capturedData);
-                  console.log('result captured :: ');
-              } else {
-                console.log('no method matched :: ', request.method());
-              }
-            } else {
+    if (pageObj.captureURLs) {
+      if (_.some(pageObj.captureURLs, function(url) { return request.url().indexOf(url) > -1 ? true : false;})) {
+        if (pageObj.captureMethods) {
+          if (_.some(pageObj.captureMethods, function(method) { return method == request.method();})) {
               result.push(capturedData);
-              console.log('result captured :: ');
-            }
-          }
-        } else {
-          if (pageObjConfig.captureMethods) {
-            if (_.some(pageObjConfig.captureMethods, function(method) { method == request.method();})) {
-                result.push(capturedData);
-                console.log('result captured :: ');
-            }
           } else {
-            result.push(capturedData);
-            console.log('result captured :: ');
+            console.log('no method matched :: ', request.method());
           }
-        }      
-      request.continue();
-    }).catch(error => {
-      //console.error(error);
-      request.abort();
-    });
-  }); 
-
-  //(async() => {
-    let pageObjects = _.sortBy(configObj.pageObjects, 'pageIndex');
-    let lastIndex = pageObjects.length;
-    console.log('pageObjects sorted :: ', lastIndex);
-
-    for (i=0; i<lastIndex; i++) {
-      let pageObj = pageObjects[i];
-      pageObjConfig = pageObj;
-      console.log('going to :: ' + pageObj.pageURL);
-      await page.goto(pageObj.pageURL, {"waitUntil" : "networkidle0"});
-      page.waitForNavigation();
-      await page.bringToFront();
-      let formData = pageObj.formData || [];
-      let formDataIndex = formData.length;
-      formData.forEach(function(obj) {
-        console.log(formDataIndex, obj.key, obj.value);
-        if ('dropdown' == obj.type) {
-          (async() => {
-           page.select('#' + obj.key, obj.value);
-           formDataIndex = formDataIndex - 1;
-           if (formDataIndex <= 0) {
-            console.log('before click ::', '#' + pageObj.actionEventId);
-              if ('Submit' == pageObj.pageAction) {
-                await page.keyboard.press('Enter');
-                await page.waitForNavigation({waitUntil: 'networkidle2'});
-              }
-           }
-           })();
         } else {
-          (async() => {
-          await page.type('#' + obj.key, obj.value);
-           formDataIndex = formDataIndex - 1;
-           if (formDataIndex <= 0) {
-              console.log('before click ::', '#' + pageObj.actionEventId);
-              if ('Submit' == pageObj.pageAction) {
-                    await page.keyboard.press('Enter');
-                    //page.$eval('#' + pageObj.actionEventId, elem => elem.click());
-                    await page.waitForNavigation({waitUntil: 'networkidle2'});            
-              }       
-          }
-          })();
+          result.push(capturedData);
         }
-      });
-    }
-    try {
-      await page.waitForNavigation({waitUntil: 'networkidle2'});
-      const finalResponse = await page.waitForResponse(response =>  response.status() === 200);
-      console.log('finalResponse :: ', finalResponse);
-      await Promise.race([
-        new Promise(resolve => {
-          setTimeout(resolve, 20000);
-        }),
-        new Promise(resolve => {
-          setTimeout(resolve, 20000)
-        })
-      ]);
-      console.log('################# ::' + pendingXHR.pendingXhrCount());
-      callback({
-        status: 'SUCCESS',
-        result: result
-      });
-    } catch(e) {
-      console.log('error while await timeout ::: ');
-      callback({
-        status: 'SUCCESS',
-        result: result
-      });
-    }
-    
-  })();
-}
-
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function pageDataLoader(page, pageObj) {
-  let formData = pageObj.formData;
-  let lastIndex = formData.length || 0;
-  return new Promise(resolve => {
-    //setTimeout(() => {
-      formData.forEach(function(obj) {
-        if ('dropdown' == obj.type) {
-           page.select('#' + obj.key, obj.value);
-           lastIndex = lastIndex - 1;
-           if (lastIndex <= 0) {
-            resolve('resolved');
-           }
-        } else if ('input' == obj.type) {
-           lastIndex = lastIndex - 1;
-           if (lastIndex <= 0) {
-            resolve('resolved');
-           }
-        }
-      });
-      
-    //}, 2000);
-  });
-}
-
-/*async function pageDataLoader(page, pageObj, callback) {
-  let formData = pageObj.formData;
-  let lastIndex = formData.length || 0;
-
-  if (lastIndex) {
-    formData.forEach(function(obj) {
-      if ('dropdown' == obj.type) {
-         page.select('#' + obj.key, obj.value);
-         lastIndex = lastIndex - 1;
-         if (lastIndex <= 0) {
-          callback('done');
-         }
-      } else if ('input' == obj.type) {
-         page.type('#' + obj.key, obj.value);
-         page.$eval('#' + pageObj.actionEventId, elem => elem.click());
-         lastIndex = lastIndex - 1;
-         if (lastIndex <= 0) {
-          callback('done');
-         }
+      } else {
+        //console.log('URL NOT MATCHED @@@@@ ', _.some(pageObj.captureURLs, function(url) { return request.url().indexOf(url)}));
       }
+    } else {
+      if (pageObj.captureMethods) {
+        if (_.some(pageObj.captureMethods, function(method) { method == request.method();})) {
+            result.push(capturedData);
+        }
+      } else {
+        result.push(capturedData);
+      }
+    } 
+  });
+  if (pageObj.pageURL) {
+    await page.goto(pageObj.pageURL);
+  }
+  page.waitForNavigation({"waitUntil" : ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']});
+  page.bringToFront();
+
+  let formData = pageObj.formData || [];
+  let formDataIndex = formData.length;
+  
+    formData.forEach(function(obj) { 
+      (async() => {
+        await formFiller(page, obj);
+      })();
     });
+  
+  if ('Submit' == pageObj.pageAction) {
+    if (pageObj.actionEventSelector) {
+      await page.click(pageObj.actionEventSelector);
+      await page.waitForNavigation({waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']});
+    } else if (pageObj.actionEventId) { 
+      page.bringToFront();     
+      await page.$eval('#'+pageObj.actionEventId, element => element.click());
+      await page.click('[id="'+pageObj.actionEventId+'"]');
+      await page.keyboard.press("Tab");
+      await page.keyboard.press("Enter");
+      console.log('SUBMITTED ::: ');
+      page.waitForNavigation({waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']});
+    } else {
+      await page.keyboard.press('Enter');
+      await page.waitForNavigation({waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']});
+    }
+  }
+  console.log('PAGE pageGraber DONE :: ');
+}
+
+
+async function formFiller(page, obj) {
+  page.waitForSelector('#' + obj.key);
+  if ('dropdown' == obj.type) {
+    page.select('#' + obj.key, obj.value);
+  } else if ('radio' == obj.type) {
+
+  } else if ('checkbox' == obj.type) {
+
   } else {
-    callback('done');
-  }  
-}*/
+    page.type('#' + obj.key, obj.value);
+  }
+}
 
+function delay() {
+  return new Promise(resolve=>setTimeout(resolve,5000));
+}
 
-module.exports.runScript = runScript;
+module.exports.scriptLauncher = scriptLauncher;
